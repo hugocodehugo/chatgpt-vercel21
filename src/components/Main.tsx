0fb1d5b16f0fe2b5f0223c28673fabe97be28bb1
hugocodehugo <124509560+hugocodehugo@users.noreply.github.com>
2023-03-21 01:36:31 +0800

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -305,14 +305,14 @@ export default function (props: {
   const find = throttle(
     (value: string) => {
       if (value === "/" || value === " ")
-        return setCompatiblePrompt(props.prompts.slice(0, 60))
+        return setCompatiblePrompt(props.prompts.slice(0, 100))
       const query = value.replace(/^[\/ ](.*)/, "$1")
       if (query !== value)
         setCompatiblePrompt(
           fzf
             .find(query)
             .map(k => k.item)
-            .slice(0, 60)
+            .slice(0, 100)
         )
     },
     250,
